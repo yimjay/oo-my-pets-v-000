2e9e1c1eeca39c303dc022aea636e55737ba6d58
Ji Yim <gsooyim@gmail.com>
2018-05-02 04:48:59 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,7 @@
 class Owner
   attr_accessor :pets
   
+  
   def initialize
     @pets = {
       fishes: [],
@@ -11,7 +12,7 @@ class Owner
     
   def buy_cat(name)
     new_cat = Cat.new(name)
-    @pets[cats] = new_cat
+    @pets[cats] << new_cat
   end
   
   def buy_dog(name)
