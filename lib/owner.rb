6d35ef1058eb1632d21f87fc4a61bf8a5fd6ac6b
Ji Yim <gsooyim@gmail.com>
2018-05-02 04:42:33 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -7,4 +7,16 @@ class Owner
       cats: [],
       dogs: []
     }
+    
+  def buy_cat(name)
+    new_cat = Cat.new(name)
+    @pets[cats] = 
+  end
+  
+  def buy_dog(name)
+  end
+  
+  def buy_fish(name)
+  end
+
 end
